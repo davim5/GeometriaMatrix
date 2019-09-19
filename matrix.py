eafc1fc2014a5119ceb49c1f260d00c252d525ee
Davi Lima <davim53@hotmail.com>
2019-09-18 23:54:05 -0300

    função transpose funcionando

--- matrix.py
+++ matrix.py
@@ -164,7 +164,7 @@ class Matrix:
 
 #Cij += Aik * Bkj
 
-    def _dot_(self,other):
+    def dot(self,other):
         if self.cols != other.rows:
             print('Número de linhas e colunas é incompatível para multiplicação!')
             return
@@ -176,6 +176,16 @@ class Matrix:
                     for j in range(1,other.cols + 1):
                         res[i,j] += self[i,k] * other[k,j]
             return res
+
+    def transpose(self):
+
+        res = Matrix(self.cols,self.rows)
+
+        for i in range(1, self.rows+1):
+            for j in range(1, self.cols+1):
+                res[j,i] = self[i,j]
+
+        return res
 
 
     def gauss_jordan(self):
@@ -231,127 +241,5 @@ class Matrix:
                         
         return res
 
-    # def solve(self):
-    #     #checa se a matriz tem a ultima coluna "b" para que as operações sejam feitas
-    #     if self.cols == self.rows + 1:
-    #         #Variável recebe a raiz passada na função
-    #         res = self
-    #         #Checar se os termos abaixo da diagonal principal estão zerados
-    #         for j in range(1,self.cols): #Colunas menos a ultima (1 a 3)
-    #             print('loop 1')
-
-    #             for i in range(self.rows,j,-1): #Linhas até antes da diagonal principal (3 a 1)
-    #                 print('loop 2 --- ','i = ',i,' j = ',j)
-    #                 print('res[i,j] = ',res[i,j])
-
-    #                 # if(res[j,j] == 0):
-    #                 #     aux = res
-    #                 #     for x in range(1, self.cols):
-    #                 #         res[j-1,x] = aux[j,x]
-
-    #                 if res[i,j] != 0: #Se encontrar um que não seja 0 (i = linha, j = coluna)
-
-    #                     for k in range(1, self.rows + 1): #k = variavel pra checar outras linhas da mesma coluna 'j'
-    #                         print('loop 3 --- k = ',k)
-    #                         print('res[k,j] = ',res[k,j])
-    #                         print(res.data,'\n')
-
-    #                         al = k
-
-    #                         if(res[i,j-1] == 0):
-    #                             print('Leu que o antes é zero')
-    #                             while(res[al,j-1] != 0):
-    #                                 al += 1
-    #                                 print(al)
-
-    #                         print(al)
-    #                         if res[i,j] == res[al,j] and i != al and res[i,j] != 0: #Se for igual e não for a mesma linha
-    #                             print('al = ', al)
-
-    #                             for s in range (1,self.cols+1): #s = variavel pra pegar cada elemento da linha 'i' e subtrair com a linha 'k' até chegar na diagonal
-    #                                 print('loop 4 == --- s = ',s)
-    #                                 res[i,s] -= res [al,s]
-    #                                 #resultado deve ser zero
-    #                             print('\n')            
-    #                         elif res[i,j] < res[al,j] and res[i,j] != 0: #Se lemento da outra linha for maior
-    #                             print('al = ', al)
-    #                             if res[al,j]%res[i,j] == 0:
-    #                                 div = res[al,j]/res[i,j]
-    #                                 for s in range(1,self.cols+1):
-    #                                     print('loop 4 < --- s = ',s)
-    #                                     res[i,s] -= res[al,s]/div
-    #                                 print('\n')
-    #                         elif res[i,j] > res[al,j]: #Se lemento da outra linha for maior
-    #                             print('al = ', al)
-    #                             if res[i,j]%res[al,j] == 0:
-    #                                 mult = res[i,j]/res[al,j]
-    #                                 for s in range(1,self.cols+1):
-    #                                     print('loop 4 > --- s = ',s)
-    #                                     res[i,s] -= res[al,j]*mult
-    #                                 print('\n')
-    #         for j in range(1,self.cols): #Colunas menos a ultima (1 a 3)
-    #             print('loop 1')
-
-    #             for i in range(self.rows,j,-1): #Linhas até antes da diagonal principal (3 a 1)
-    #                 print('loop 2 --- ','i = ',i,' j = ',j)
-    #                 print('res[i,j] = ',res[i,j])
-
-    #                 # if(res[j,j] == 0):
-    #                 #     aux = res
-    #                 #     for x in range(1, self.cols):
-    #                 #         res[j-1,x] = aux[j,x]
-    #                 if res[i,j] != 0: #Se encontrar um que não seja 0 (i = linha, j = coluna)
-
-    #                     for k in range(1, self.rows + 1): #k = variavel pra checar outras linhas da mesma coluna 'j'
-    #                         print('loop 3 --- k = ',k)
-    #                         print('res[k,j] = ',res[k,j])
-    #                         print(res.data,'\n')
-
-    #                         al = k
-
-    #                         if(res[i,j-1] == 0):
-    #                             print('Leu que o antes é zero')
-    #                             while(res[al,j-1] != 0):
-    #                                 al += 1
-    #                                 print(al)
-
-    #                         print(al)
-    #                         if res[i,j] == res[al,j] and i != al and res[i,j] != 0: #Se for igual e não for a mesma linha
-    #                             print('al = ', al)
-
-    #                             for s in range (1,self.cols+1): #s = variavel pra pegar cada elemento da linha 'i' e subtrair com a linha 'k' até chegar na diagonal
-    #                                 print('loop 4 == --- s = ',s)
-    #                                 res[i,s] -= res [al,s]
-    #                                 #resultado deve ser zero
-    #                             print('\n')            
-    #                         elif res[i,j] < res[al,j] and res[i,j] != 0: #Se lemento da outra linha for maior
-    #                             print('al = ', al)
-    #                             if res[al,j]%res[i,j] == 0:
-    #                                 div = res[al,j]/res[i,j]
-    #                                 for s in range(1,self.cols+1):
-    #                                     print('loop 4 < --- s = ',s)
-    #                                     res[i,s] -= res[al,s]/div
-    #                                 print('\n')
-    #                         elif res[i,j] > res[al,j]: #Se lemento da outra linha for maior
-    #                             print('al = ', al)
-    #                             if res[i,j]%res[al,j] == 0:
-    #                                 mult = res[i,j]/res[al,j]
-    #                                 for s in range(1,self.cols+1):
-    #                                     print('loop 4 > --- s = ',s)
-    #                                     res[i,s] -= res[al,j]*mult
-    #                                 print('\n')
-    #     return res
-
-            #Zerar termos abaixo da diagonal principal OOOOK
-
-                #sendo 'm' e 'n', respectivamente, os ultimos valores de linhas e colunas OKOOK
-                #Zerar primeiro res[m,n] até res[n+1,n] (EXEMPLO: [4,1] até [2,1], depois [4,2] até [3,2]... Sempre da primeira linha da coluna até o 'meio'. OOOKOK
-
-            #Checar se os termos da diagonal principal são '1'
-
-            #Transformar os termos da diagonal principal para '1'
-
-            #Zerar os termos acima da diagonal principal.
-# diosaidos
     #self.data[(j-1) + (i-1) * self.cols] = value
     #no prompt: 'python'(na pasta do arquivo), 'from matrix import Matrix', 'exit' , 'exit()'
