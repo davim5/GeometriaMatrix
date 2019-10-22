2039fcee9ba1f456a95a741899bba29eb8f11bdb
Davi Lima <davim53@hotmail.com>
2019-10-22 13:57:38 -0300

    Adicionando classes retangulo e triangulo

--- opengl/cube.py
+++ opengl/cube.py
@@ -3,6 +3,8 @@ from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from shape2d import Square
+from shape2d import Rectangle
+from shape2d import Triangle
 
 # def cube():
 #   glBegin(GL_LINES)
@@ -13,11 +15,13 @@ from shape2d import Square
 
 if __name__ == "__main__":
   pygame.init()
-  display = (500,500)
+  display = (720,720)
   pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
   gluPerspective(45,display[0]/display[1],0.1,50.0)
   glTranslate(0.0,0.0,-35)
-  square = Square(1,1,10)
+  square = Square(-10,0,10)
+  rectangle = Rectangle(-10,0,20,10)
+  triangle = Triangle(-10,0,10,10)
 
   while True:
     for event in pygame.event.get():
@@ -28,7 +32,9 @@ if __name__ == "__main__":
     
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     
-    square.draw()
+    # square.draw()
+    # rectangle.draw()
+    triangle.draw()
 
     pygame.display.flip()
     pygame.time.wait(10)
--- opengl/shape2d.py
+++ opengl/shape2d.py
@@ -1,5 +1,6 @@
 from OpenGL.GL import *
 
+# Classe do quadrado
 class Square():
   def __init__(self,x,y,width):
     self.x = x
@@ -19,7 +20,7 @@ class Square():
   def draw(self):
     glBegin(GL_TRIANGLES)
 
-    glColor(1.0,0.0,0.0)
+    glColor(0.0,0.0,1.0)
     glVertex2fv(self.points[0])
     glVertex2fv(self.points[1])
     glVertex2fv(self.points[2])
@@ -28,6 +29,67 @@ class Square():
     glVertex2fv(self.points[2])
     glVertex2fv(self.points[0])
     glVertex2fv(self.points[3])
+
+    glEnd()
+
+# Classe do retangulo
+class Rectangle():
+  def __init__(self,x,y,width,height):
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+    self.points = self._create_points()
+
+
+  def _create_points(self):
+    points_list = []
+    points_list.append((self.x,self.y))
+    points_list.append((self.x + self.width,self.y))
+    points_list.append((self.x + self.width,self.y + self.height))
+    points_list.append((self.x,self.y + self.height))
+    return points_list
+
+  def draw(self):
+    glBegin(GL_TRIANGLES)
+
+    glColor(0.0,0.0,1.0)
+    glVertex2fv(self.points[0])
+    glVertex2fv(self.points[1])
+    glVertex2fv(self.points[2])
+
+    glColor(0.0,1.0,0.0)
+    glVertex2fv(self.points[2])
+    glVertex2fv(self.points[0])
+    glVertex2fv(self.points[3])
+
+    glEnd()
+
+# Classe do quadrado
+class Triangle():
+  def __init__(self,x,y,width,height):
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+    self.points = self._create_points()
+
+
+  def _create_points(self):
+    points_list = []
+    points_list.append((self.x,self.y))
+    points_list.append((self.x + self.width,self.y))
+    # points_list.append((self.x + self.width,self.y + self.width))
+    points_list.append((self.x + self.width/2,self.y + self.height))
+    return points_list
+
+  def draw(self):
+    glBegin(GL_TRIANGLES)
+
+    glColor(0.0,0.0,1.0)
+    glVertex2fv(self.points[0])
+    glVertex2fv(self.points[1])
+    glVertex2fv(self.points[2])
 
     glEnd()
     
